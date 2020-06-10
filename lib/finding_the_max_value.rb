7ee1e94f6cefb0f4b166826f3fff64662f0fea2e
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-10 01:50:45 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -6,6 +6,10 @@ def find_max_value(array)
   while count < array.length do
     if max_value < array[count]
       max_value = array[count]
+<<<<<<< HEAD
+=======
+      binding.pry
+>>>>>>> f65f9c01629a670087ed46e5b9a1909aaf156081
     end
     count += 1
   end  
